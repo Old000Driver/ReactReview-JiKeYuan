42c80d2530a9539812bccb904b9ff499a9d0a988
GJA <792002710@qq.com>
2024-12-03 18:03:19 +0800

    feat(login): 实现登录页面功能
    - 添加登录页面组件，包含表单验证和提交功能
    - 集成 Ant Design 组件库
    - 添加全局样式文件
    - 更新路由配置，支持登录页面路由

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,48 @@
-const Login = () =>{
-  return <div>this is Login</div>
+import { Card } from 'antd'
+import logo from '@/assets/logo.png'
+import './index.scss'
+import { Form, Input, Button } from 'antd'
+
+const Login = () => {
+  const onFinish = formValue => {
+    console.log('formValue',formValue)
+  }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger={['onBlur', 'onChange']} onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对',
+                validateTrigger: 'onBlur'
+              },
+              { required: true, message: '请输入手机号' }
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              { required: true, message: '请输入验证码' },
+              { len: 6, message: '验证码6个字符', validateTrigger: 'onBlur' }
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,4 @@
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
@@ -20,7 +14,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-export default router
+export default router;
 
 // createRoot(document.getElementById("root")).render(
 //   <RouterProvider router={router} />
