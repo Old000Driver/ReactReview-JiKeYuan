8dbe3110720068efa4d019a14fac80b04445db5b
GJA <792002710@qq.com>
2024-12-04 20:26:09 +0800

    refactor(Home): 重构首页图表展示
    - 移除直接使用 echarts 的代码
    - 引入独立的 BarChart 组件
    - 增加两个 BarChart 组件的展示

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,9 @@
-import * as echarts from "echarts";
-import { useEffect, useRef } from "react";
-
+import BarChart from "./components/BarChart";
 const Home = () => {
-  const chartRef = useRef(null);
-  useEffect(() => {
-    const chartDom = chartRef.current;
-    const myChart = echarts.init(chartDom);
-    let option;
-
-    option = {
-      xAxis: {
-        type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [
-        {
-          data: [120, 200, 150, 80, 70, 110, 130],
-          type: "bar",
-        },
-      ],
-    };
-
-    option && myChart.setOption(option);
-  }, []);
   return (
     <div>
-      <div
-        id="main"
-        ref={chartRef}
-        style={{ width: "500px", height: "400px" }}
-      ></div>
+      <BarChart title={"三大框架满意度"} />
+      <BarChart title={"三大框架使用度"} />
     </div>
   );
 };
