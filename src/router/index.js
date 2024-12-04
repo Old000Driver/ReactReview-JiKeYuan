604acfc3fa3089b7d27ad86578664bb31132528b
GJA <792002710@qq.com>
2024-12-04 17:17:17 +0800

    feat(layout): 新增 GeekLayout 组件
    - 添加 normalize.css 以重置浏览器默认样式
    - 创建 GeekLayout 组件，包含 Header、Sider 和 Content 区域
    - 集成 Ant Design 组件库
    - 添加路由配置，支持首页、文章管理和创建文章页面
    - 调整全局样式，设置 html 和 body 高度为 100%

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
 import AuthRoute from "@/components/AuthRoute";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 const router = createBrowserRouter([
   {
@@ -12,7 +15,23 @@ const router = createBrowserRouter([
         <Layout />
       </AuthRoute>
     ),
+    children: [
+      {
+        index: true,
+        // path: "home",
+        element: <Home />,
+      },
+      {
+        path: "article",
+        element: <Article />,
+      },
+      {
+        path: "publish",
+        element: <Publish />,
+      },
+    ],
   },
+
   {
     path: "/login",
     element: <Login />,
