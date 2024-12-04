d4e6c54a903b9a1236d8857c1180e206ad336cdb
GJA <792002710@qq.com>
2024-12-04 20:19:36 +0800

    feat: 在首页添加 echarts 图表
    - 在 package.json 中添加 echarts 依赖
    - 在 Home 组件中集成 echarts，创建柱状图
    - 添加 useEffect 钩子用于初始化图表
    - 使用 useRef 钩子获取图表容器的引用

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,40 @@
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+
 const Home = () => {
-  return <div>this is home</div>;
+  const chartRef = useRef(null);
+  useEffect(() => {
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+    let option;
+
+    option = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: "bar",
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }, []);
+  return (
+    <div>
+      <div
+        id="main"
+        ref={chartRef}
+        style={{ width: "500px", height: "400px" }}
+      ></div>
+    </div>
+  );
 };
 
 export default Home;
