3a80d640bdc07edecbb1630d8cb934fd63b94ec8
GJA <792002710@qq.com>
2024-12-04 15:52:01 +0800

    feat(Layout): 添加用户信息请求
    - 在 Layout 组件中添加了获取用户信息的请求
    - 优化了 request.js 文件中的请求拦截器，添加了 token 认证

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,5 +1,10 @@
-const Layout = () =>{
-  return <div>this is Layout</div>
-}
+import { request } from "axios";
+import { useEffect } from "react";
+const Layout = () => {
+  useEffect(() => {
+    request.get("/user/profile");
+  });
+  return <div>this is Layout</div>;
+};
 
-export default Layout
+export default Layout;
